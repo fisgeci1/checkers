807c938f670bbd0063afdb6730c1eb8cf66e6097
fisgeci1 <fisgeci1>
2019-05-18 00:45:21 +0200

    win Condition added
    Added win condition
    and improved the code

--- Board.java
+++ Board.java
@@ -3,39 +3,40 @@ import java.awt.*;
 
 public class Board extends JPanel {
 
-    private static  final int col = 8;
-    private MoveChecker moveChecker ;
+
+    private static final int col = 8;
+    private MoveChecker moveChecker;
     private Tilles[][] tilles = new Tilles[col][col];
-    private int width ,height;
-    public Board(MoveChecker moveChecke,int width,int height){
+    private int width, height;
+
+    public Board(MoveChecker moveChecke, int width, int height) {
 
         this.width = width;
         this.height = height;
-        setSize(width,height);
-        GridLayout grid = new GridLayout(col,col);
+        setSize(width, height);
+        GridLayout grid = new GridLayout(col, col);
         setLayout(grid);
         this.moveChecker = moveChecke;
 
         createTable(grid);
 
 
-
-
     }
 
-private void createTable(GridLayout grid){
-        for(int i = 0;i<col;i++){
-            for(int j = 0; j<col;j++){
-                if((i+j)%2 == 0){
-                        add(tilles[i][j] = new Tilles(i,j,Color.RED,moveChecker,width/col,height/col));
-                }else{
-                    add(tilles[i][j] = new Tilles(i,j,Color.GRAY,moveChecker,width/col,height/col));
+
+    private void createTable(GridLayout grid) {
+        for (int i = 0; i < col; i++) {
+            for (int j = 0; j < col; j++) {
+                if ((i + j) % 2 == 0) {
+                    add(tilles[i][j] = new Tilles(i, j, Color.RED, moveChecker, width / col, height / col));
+                } else {
+                    add(tilles[i][j] = new Tilles(i, j, Color.GRAY, moveChecker, width / col, height / col));
                 }
 
             }
 
         }
-}
+    }
 
     public Tilles[][] getTilles() {
         return tilles;
--- Cotnroller.java
+++ Cotnroller.java
@@ -1,12 +1,11 @@
 public class Cotnroller {
 
-    public static void main(String[] args){
-            int width = 500;
-        MoveChecker move  = new MoveChecker();
-        Board board = new Board(move,width,width);
+    public static void main(String[] args) {
+        int width = 500;
+        MoveChecker move = new MoveChecker();
+        Board board = new Board(move, width, width);
         move.setTilles(board.getTilles());
-
-        Frame frame  = new Frame(board,width,width);
+        Frame frame = new Frame(board, width, width);
 
 
     }
